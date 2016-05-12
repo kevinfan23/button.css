d9bd2690a02091ea8b124854c716b3925629ba23
kevinfan23 <kevinfan23@ucla.edu>
2016-05-11 19:35:48 -0700

    update DNS

---
+++ js/main.js
@@ -0,0 +1,70 @@
+$(document).ready(function() {
+	
+	smoothScroll();
+	
+	// Animate menu using animate.css
+	$('.navbar').addClass('animated bounceInDown');
+
+	// Scroll event listen
+	$(window).on('scroll', function(){
+		updateNavigation();
+	});
+	
+	// Update nav selected when click
+	$('a').on('click', function() {
+		console.log("haha");
+		$('.nav-item').removeClass('active');
+		$(this).parent().addClass('active');
+	});
+});
+
+// Smooth the scroll action
+function smoothScroll() {
+	
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        return false;
+      }
+    }
+  });
+}
+
+// Update nav selected
+function updateNavigation() {
+	var lastId,
+    topMenu = $(".navbar"),
+    topMenuHeight = topMenu.outerHeight()+15,
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function(){
+      var item = $($(this).attr("href"));
+      if (item.length) { return item; }
+    });
+    
+   // Get container scroll position
+   var fromTop = $(this).scrollTop()+topMenuHeight;
+   
+   // Get id of current scroll item
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+   // Get the id of the current element
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+   
+   if (lastId !== id) {
+       lastId = id;
+       // Set/remove active class
+       menuItems
+         .parent().removeClass("active")
+         .end().filter("[href='#"+id+"']").parent().addClass("active");
+   }                   
+}
